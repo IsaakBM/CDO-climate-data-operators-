00756ba0b41d1793fc806c64460ba8f02e4a7fc2
Isaac Brito <ibritomorales@gmail.com>
2020-02-17 12:08:34 +1000

    minor update

--- bash_scripts/Regrid AVHRSST(2).R
+++ bash_scripts/Regrid AVHRSST(2).R
@@ -26,10 +26,37 @@
 	registerDoParallel(cl)
 	
 	foreach(i = 1:length(f))  %dopar% { 
-	  ifile <- paste0(ipath, f[i])
+	  ifile <- paste0(ipath, f[1])
 	  grdfile <- paste0(ipath, grd)
 	  ofile <- paste0(opath, f[i])
 	  # system(paste("cdo yearmean", ifile, ofile, sep = (" ")))
 	  system(paste(paste("cdo remapbil,", grdfile, ",", sep = ""), ifile, ofile, sep = (" ")))
 	}
+	
+	
+	ipath <- "/Users/bri273/Desktop/testing" # Input path
+	opath <- "/Users/bri273/Desktop/testing/" # Output path
+	dir.nc <- paste(list.dirs(path = ipath, full.names = TRUE, recursive = FALSE))
+	
+	for(i in 1:length(dir.nc)) {
+	  
+	  file.grd <- paste(dir.nc[i], list.files(path = paste(dir.nc[i], sep = "/"), pattern = "*.grd"), sep = "/")
+	  files.nc <- paste(dir.nc[i], list.files(path = paste(dir.nc[i], sep = "/"), pattern = "*.nc"), sep = "/")
+	  
+	  UseCores <- detectCores() -1
+	  cl <- makeCluster(UseCores)  
+	  registerDoParallel(cl)
+	  
+	  foreach(j = 1:length(files.nc)) %dopar% {
+	    
+	    system(paste(paste("cdo remapbil,", file.grd, ",", sep = ""), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
+	    
+	  } 
+	  
+	}
+	
+	
+	
+	
+	
 	

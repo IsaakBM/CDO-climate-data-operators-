bc34f3c5736c14e70dba046ca75cb511a810da47
Isaac Brito <ibritomorales@gmail.com>
2020-02-17 12:41:42 +1000

    old files

--- bash_scripts/cdo_bilweights.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# AIM: Construct weights for bilinear interpolation
-
-# use genbil to construct weights for bilinear interpolation using a 1°deg grid
-cdo genbil,1deg.grd, InputFile.nc OutputFile_weights.nc
-# You can also use gendis (distance-weighted average) in case of an error in the interpolation weights:
-    # Constructing weights for bilinear interpolation using gendis and a "generic" grid at 0.5° resolution
-    # cdo gendis,r720x360, InputFile.nc OutputFile_weights.nc

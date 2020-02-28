efb7cd66fb72138308f840c7f442d9a1b6589b42
Isaac Brito <ibritomorales@gmail.com>
2020-02-28 13:55:19 +1000

    update for HPCs

--- scripts/bash_scripts/PBS_regrid_01.sh
+++ scripts/bash_scripts/PBS_regrid_01.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #PBS -A qris-uq
-#PBS -l walltime=2:00:00
+#PBS -l walltime=1:00:00
 #PBS -l select=6:ncpus=4:mem=80GB
-        #+6:ncpus=20:mem=500GB
 
 cd $PBS_O_WORKDIR
 
-module load parallel
 module load cdo
+module load ncview
+module load geos
+module load gdal
 module load R/3.5.0
 
-R CMD BATCH "/QRISdata/Q1216/BritoMorales/zztest/regrid/regrid01.R"
+export INCLUDE=$INCLUDE:/opt/netcdf/4.3.2/gnu/openmpi_ib/lib
+
+R CMD BATCH "/QRISdata/Q1215/ClimateModels/y_test/regrid01.R"

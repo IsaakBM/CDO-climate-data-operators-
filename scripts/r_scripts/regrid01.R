88abec76558185eb97e97f42b3708d8dec4494f1
Isaac Brito <ibritomorales@gmail.com>
2020-03-03 11:32:10 +1000

    minor lines

--- scripts/r_scripts/regrid01.R
+++ scripts/r_scripts/regrid01.R
@@ -6,9 +6,16 @@ library(parallel)
 # ipath <- "/QRISdata/Q1215/ClimateModels/y_test/ssp126"
 # opath <- "/Users/bri273/Desktop/CDO/models_regrid/ssp126/" # Output path
 # opath <- "/QRISdata/Q1215/ClimateModels/y_test/ssp126/"
-ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean/thetao/ssp585"
-opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/thetao_05deg/"
 
+# ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean/thetao/ssp585"
+# opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/thetao_05deg/"
+
+ipath <- "/QRISdata/Q1215/ClimateModels/y_test/ssp245"
+opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/thetao_01deg/ssp245/"
+
+# ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean/tos_oday/ssp585"
+# opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/tos_oday_01deg/"
+  
 dir.nc <- paste(list.dirs(path = ipath, full.names = TRUE, recursive = FALSE))
 
 for(i in 1:length(dir.nc)) {
@@ -20,8 +27,8 @@ for(i in 1:length(dir.nc)) {
   registerDoParallel(cl)
   
   foreach(j = 1:length(files.nc)) %dopar% {
-    system(paste(paste("cdo -L remapbil,", file.grd, ",", sep = ""), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
-    # system(paste(paste("cdo -L remapbil,", file.grd, ",", sep = ""), paste("-selname","thetao", sep = ","), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
+    # system(paste(paste("cdo -L remapbil,", file.grd, ",", sep = ""), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
+    system(paste(paste("cdo -L remapbil,", file.grd, ",", sep = ""), paste("-selname","thetao", sep = ","), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
   } 
   stopCluster(cl)
 }

35e08d577bb208db0191cad34e77b27e432a0e54
Isaac Brito <ibritomorales@gmail.com>
2020-03-23 09:02:53 +1000

    minor updates

--- scripts/r_scripts/oceanlayers.R
+++ scripts/r_scripts/oceanlayers.R
@@ -4,8 +4,12 @@ library(parallel)
 # ipath <- "/Users/bri273/Desktop/CDO/models_regrid/ssp126" # Input path
 # opath <- "/Users/bri273/Desktop/CDO/models_regrid_layers/" # Output path
 
-ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/thetao_01deg/ssp245" # Input path
-opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid_layers/thetao_01deg/ssp245/" # Output path
+ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/thetao_05deg/ssp585" # Input path
+opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid_layers/thetao_05deg/ssp585/" # Output path
+
+ipath <- "/QRISdata/Q1215/ClimateModels/y_test/ssp585" # Input path
+opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid_layers/thetao_05deg/ssp585/" # Output path
+
 
 dir.nc <- paste(list.dirs(path = ipath, full.names = TRUE, recursive = FALSE))
 
@@ -13,19 +17,20 @@ for(i in 1:length(dir.nc)) { # number of models inside ssps scenarios
   files.nc <- paste(dir.nc[i], list.files(path = paste(dir.nc[i], sep = "/"), pattern = "*.nc"), sep = "/")
   levels <- as.vector(system(paste("cdo showlevel", files.nc[1]), intern = TRUE))
   
-  UseCores <- 22
+  lev <- unlist(strsplit(levels, split = " "))
+  depths <- unique(lev[lev != ""])
+  sf <- depths[as.numeric(depths) <= 500]
+  ep <- depths[as.numeric(depths) > 500 & as.numeric(depths) <= 20000]
+  mp <- depths[as.numeric(depths) > 20000 & as.numeric(depths) <= 100000]
+  bap <- depths[as.numeric(depths) > 100000]
+  # bp <- depths[as.numeric(depths) > 1000 & as.numeric(depths) <= 4000]
+  # abp <- depths[as.numeric(depths) > 4000]
+  
+  UseCores <- 15
   cl <- makeCluster(UseCores)  
   registerDoParallel(cl)
   
   foreach(j = 1:length(files.nc)) %dopar% {
-    lev <- unlist(strsplit(levels, split = " "))
-    depths <- unique(lev[lev != ""])
-    sf <- depths[as.numeric(depths) <= 5]
-    ep <- depths[as.numeric(depths) > 5 & as.numeric(depths) <= 200]
-    mp <- depths[as.numeric(depths) > 200 & as.numeric(depths) <= 1000]
-    bap <- depths[as.numeric(depths) > 1000]
-    # bp <- depths[as.numeric(depths) > 1000 & as.numeric(depths) <= 4000]
-    # abp <- depths[as.numeric(depths) > 4000]
     system(paste(paste("cdo -L -sellevel,", paste0(sf, collapse = ","), ",", sep = ""), paste("-selname,", "thetao", sep = ""), files.nc[j], paste0(opath, "01-sf_", basename(files.nc[j]))))
     system(paste(paste("cdo -L -sellevel,", paste0(ep, collapse = ","), ",", sep = ""), paste("-selname,", "thetao", sep = ""), files.nc[j], paste0(opath, "02-ep_", basename(files.nc[j]))))
     system(paste(paste("cdo -L -sellevel,", paste0(mp, collapse = ","), ",", sep = ""), paste("-selname,", "thetao", sep = ""), files.nc[j], paste0(opath, "03-mp_", basename(files.nc[j]))))
--- scripts/r_scripts/regrid01.R
+++ scripts/r_scripts/regrid01.R
@@ -7,11 +7,11 @@ library(parallel)
 # opath <- "/Users/bri273/Desktop/CDO/models_regrid/ssp126/" # Output path
 # opath <- "/QRISdata/Q1215/ClimateModels/y_test/ssp126/"
 
-# ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean/thetao/ssp585"
-# opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/thetao_05deg/"
+ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean/thetao/ssp245"
+opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/thetao_05deg/ssp245/"
 
-ipath <- "/QRISdata/Q1215/ClimateModels/y_test/ssp126"
-opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/thetao_01deg/ssp126/"
+# ipath <- "/QRISdata/Q1215/ClimateModels/y_test/ssp126"
+# opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/thetao_01deg/ssp126/"
 
 # ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean/tos_oday/ssp585"
 # opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/tos_oday_01deg/"
@@ -22,13 +22,15 @@ for(i in 1:length(dir.nc)) {
   file.grd <- paste(dir.nc[i], list.files(path = paste(dir.nc[i], sep = "/"), pattern = "*.grd"), sep = "/")
   files.nc <- paste(dir.nc[i], list.files(path = paste(dir.nc[i], sep = "/"), pattern = "*.nc"), sep = "/")
   
-  UseCores <- 21
+  UseCores <- 5
   cl <- makeCluster(UseCores)  
   registerDoParallel(cl)
   
   foreach(j = 1:length(files.nc)) %dopar% {
     # system(paste(paste("cdo -L remapbil,", file.grd, ",", sep = ""), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
-    system(paste(paste("cdo -L remapbil,", file.grd, ",", sep = ""), paste("-selname","thetao", sep = ","), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
+    # system(paste(paste("cdo -L remapbil,", file.grd, ",", sep = ""), paste("-selname","thetao", sep = ","), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
+    # system(paste(paste("cdo -f nc -remapbil,", file.grd, ",", sep = ""), paste("-selname","thetao", sep = ","), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
+    system(paste(paste("cdo -P 10 -remapbil,", file.grd, ",", sep = ""), paste("-selname","thetao", sep = ","), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
   } 
   stopCluster(cl)
 }

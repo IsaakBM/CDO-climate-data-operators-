02a2eb020c64a3f62fe13c5f7aef144280adb833
Isaac Brito <ibritomorales@gmail.com>
2021-04-07 21:26:23 +1000

    general function working

--- scripts/r_scripts/regrid01.R
+++ scripts/r_scripts/regrid01.R
@@ -1,62 +1,70 @@
-library(doParallel)
-library(parallel)
-library(stringr)
-library(data.table)
+# This code was written by Isaac Brito-Morales (i.britomorales@uq.edu.au)
+# Please do not distribute this code without permission.
+# NO GUARANTEES THAT CODE IS CORRECT
+# Caveat Emptor!
 
-ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean/thetao/ssp585"
-opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/thetao_05deg/ssp585/"
+# Function's arguments
+# ipath: directory where the netCDF files are located
+# opath: directory to allocate the new regrided netCDF files
+# resolution = resolution for the regrid process
 
-#ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean/tob/ssp126"
-#opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/tob_05deg/ssp126/"
-
-#ipath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean/ph/ssp126"
-#opath <- "/QRISdata/Q1215/ClimateModels/CMIP6_rclean_regrid/ph_05deg/ssp126/"
-
-dir.nc <- paste(list.dirs(path = ipath, full.names = TRUE, recursive = FALSE))
-
-for(i in 1:length(dir.nc)) {
-  file.grd <- paste(dir.nc[i], list.files(path = paste(dir.nc[i], sep = "/"), pattern = "*.grd"), sep = "/")
-  files.nc <- paste(dir.nc[i], list.files(path = paste(dir.nc[i], sep = "/"), pattern = "*.nc"), sep = "/")
+regrid <- function(ipath, opath, resolution) {
   
-  UseCores <- 2 #5
-  cl <- makeCluster(UseCores)  
-  registerDoParallel(cl)
+####################################################################################
+####### Defining the main packages (tryining to auto this)
+####################################################################################
+  # List of pacakges that we will be used
+  list.of.packages <- c("doParallel", "parallel", "stringr", "data.table")
+  # If is not installed, install the pacakge
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+  # Load packages
+  lapply(list.of.packages, require, character.only = TRUE)
   
-  foreach(j = 1:length(files.nc)) %dopar% {
-     system(paste(paste("cdo -P 2 -remapbil,", file.grd, ",", sep = ""), paste("-selname","thetao", sep = ","), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
-    # system(paste(paste("cdo -P 10 -remapbil,", file.grd, ",", sep = ""), paste("-selname","tob", sep = ","), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
-    #system(paste(paste("cdo -P 10 -remapbil,", file.grd, ",", sep = ""), paste("-selname","ph", sep = ","), files.nc[j], paste0(opath, basename(files.nc[j])), sep = (" ")))
-  } 
-  stopCluster(cl)
+####################################################################################
+####### 
+####################################################################################
+  # 
+    line1 <- paste(noquote("find"), noquote(ipath), "-type", "f", "-name", noquote("*.nc"), "-exec", "ls", "-l", "{}")
+    line2 <- paste0("\\", ";")
+    line3 <- paste(line1, line2)
+  # 
+    dir_files <- system(line3, intern = TRUE)
+    dir_vector <- strsplit(x = dir_files, split = " ")
+    dir_nc <- lapply(dir_vector, function(x) x[13])
+  # 
+    final_nc <- lapply(dir_nc, function(x) {
+      c1 <- str_split(unlist(x), pattern = "//")
+      c2 <- paste(c1[[1]][1], c1[[1]][2], sep = "/")})
+    files.nc <- unlist(final_nc)
+  # 
+    var_obj <- system(paste("cdo -showname", final_nc[1]), intern = TRUE)[1]
+    var <- str_replace_all(string = var_obj, pattern = " ", replacement = "")
+ 
+####################################################################################
+####### 
+#################################################################################### 
+  # 
+    if(resolution == "1") {
+      grd <- "r360x180"
+    } else if(resolution == "0.5") {
+      grd <- "r720x360"
+    } else if(resolution == "0.5") {
+      grd <- "r1440x720"
+    }
+    
+  # 
+    UseCores <- 3
+    cl <- makeCluster(UseCores)  
+    registerDoParallel(cl)
+    foreach(j = 1:length(files.nc)) %dopar% {
+      system(paste(paste("cdo -remapbil,", grd, ",", sep = ""), 
+                   paste("-selname",var, sep = ","), files.nc[j], 
+                   paste0(opath, basename(files.nc[j])), sep = (" ")))
+    }
+    stopCluster(cl)
 }
 
-# module load R/3.5.0-intel gurobi/9.0.2
-# cdo remapbil,1deg.grd, -selname,thetao [paste("-selname","thetao", sep = ",")]
-# 
-ipath <- "/Users/bri273/Desktop/CDO/models_raw/ssp126/ACCESS-CM2/"
-ipath <- "models_raw/ssp126/"
-
-line1 <- paste(noquote("find"), noquote(ipath), "-type", "f", "-name", noquote("*.nc"), "-exec", "ls", "-l", "{}")
-line2 <- paste0("\\", ";")
-line3 <- paste(line1, line2)
-
-dir_files <- system(line3, intern = TRUE)
-dir_vector <- strsplit(x = dir_files, split = " ")
-dir_nc <- lapply(dir_vector, function(x) x[13])
-
-final_nc <- lapply(dir_nc, function(x) {
-  c1 <- str_split(unlist(x), pattern = "//")
-  c2 <- paste(c1[[1]][1], c1[[1]][2], sep = "/")
-})
-final_nc <- unlist(final_nc)
-
-system(paste("cdo -sinfo", final_nc[1]))
-
-
-
-a <- system(paste("cdo -showname", final_nc[1]), intern = TRUE)
-a <- " thetao"
-var <- str_replace_all(string = a, pattern = " ", replacement = "")
-
-
-
+regrid(ipath = "/Users/bri273/Desktop/CDO/models_raw/ssp126/ACCESS-CM2/", 
+       opath = "/Users/bri273/Desktop/CDO/models_raw/", 
+       resolution = "1")
